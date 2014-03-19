00d3b23224a7a26e429c84d2607d2320982d1442
gf0842wf <gf0842wf@gmail.com>
2014-03-19 14:33:52 +0800

    注释

--- lib/utils.py
+++ lib/utils.py
@@ -1,9 +1,2 @@
 # -*- coding: utf-8 -*-
 
-import logging as log
-
-class LogMixin(object):
-    def msg(self, *arg, **kwargs):
-        log.warning(*arg, **kwargs)
-
-__all__ = ["log", "LogMixin"]
--- server.py
+++ server.py
@@ -11,10 +11,10 @@ from tornado.options import define, options
 define("port", 8888, help="define port", type=int) # tornado没有的命令要通过define定义(logging等命令有了)
 
 if __name__ == "__main__":
-    options.logging = "debug"
+    options.logging = "debug"           # *1
     options.log_file_prefix = "var/log/test_log@8888.log"
     options.parse_command_line()
     application.listen(options.port)
     logging.debug("ioloop ... ")
     tornado.ioloop.IOLoop.instance().start()
-    # python server.py -port=8888 -logging=warning  
+    # python server.py -port=8888 -logging=warning  # 这里的会覆盖 *1行的
